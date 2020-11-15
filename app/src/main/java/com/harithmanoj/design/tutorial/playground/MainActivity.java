f6e2b61ded262f8868a8225106898fad51e2459d
Harith Manoj <harithpub@gmail.com>
2020-11-15 14:46:11 +0530

    added new activity

--- app/src/main/java/com/harithmanoj/design/tutorial/playground/MainActivity.java
+++ app/src/main/java/com/harithmanoj/design/tutorial/playground/MainActivity.java
@@ -2,10 +2,13 @@ package com.harithmanoj.design.tutorial.playground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.harithmanoj.design.tutorial.playground.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-        
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
