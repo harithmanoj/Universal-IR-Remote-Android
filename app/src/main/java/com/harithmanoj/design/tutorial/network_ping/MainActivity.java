177cab61946f5e1074dfff6c7187d0062a1ca8ce
Harith Manoj <harithpub@gmail.com>
2020-11-15 17:06:53 +0530

    changed to ping high, ping low, toasting and sequence

--- app/src/main/java/com/harithmanoj/design/tutorial/network_ping/MainActivity.java
+++ app/src/main/java/com/harithmanoj/design/tutorial/network_ping/MainActivity.java
@@ -3,6 +3,8 @@ package com.harithmanoj.design.tutorial.network_ping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void pingNow() {
-        Toast.makeText(getBaseContext(), "Pinging the pair now!!!", Toast.LENGTH_SHORT).show();
+    public void pingHigh(View view) {
+        pingNow(1);
     }
+
+    public void pingLow(View view) {
+        pingNow(0);
+    }
+
+    public void pingNow(int var) {
+        Toast.makeText(getApplicationContext(), "Pinging the pair with "
+                + String.valueOf(var) + " now !!!", Toast.LENGTH_SHORT).show();
+        TextView tv = (TextView)findViewById(R.id.textView);
+        CharSequence text = tv.getText();
+        tv.setText(text + String.valueOf(var));
+    }
+
+
 }
