27c4a888d9e5d985b959e0c3cf7d060021cbe3d5
Harith Manoj <harithpub@gmail.com>
2020-11-21 14:00:58 +0530

    completed UI

--- app/src/main/java/com/gectcr/ece/design/tutorial/networktest/NetworkDetect.java
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/NetworkDetect.java
@@ -3,8 +3,17 @@ package com.gectcr.ece.design.tutorial.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class NetworkDetect extends AppCompatActivity {
+
+    NetworkManager _networkManager;
+
+    private TextView _service;
+    private Handler _updateHandler;
+
+    public static final String TAG = "NetworkDetect";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
