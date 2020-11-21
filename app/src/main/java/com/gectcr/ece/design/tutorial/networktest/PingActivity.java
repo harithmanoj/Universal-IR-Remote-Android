be94d2f08c4fd19683492e77b265f58f4388c623
Harith Manoj <harithpub@gmail.com>
2020-11-21 16:49:02 +0530

    added ping activity and its UI

---
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/PingActivity.java
@@ -0,0 +1,14 @@
+package com.gectcr.ece.design.tutorial.networktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ping);
+    }
+}
