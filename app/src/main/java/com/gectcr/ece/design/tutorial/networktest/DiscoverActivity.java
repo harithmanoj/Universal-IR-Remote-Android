642809c70f139834edee3afe27f2f6eaa247b9d2
Harith Manoj <harithpub@gmail.com>
2020-11-22 14:04:29 +0530

    added all variables and added class for listenng to spinner ui

--- app/src/main/java/com/gectcr/ece/design/tutorial/networktest/DiscoverActivity.java
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/DiscoverActivity.java
@@ -2,13 +2,54 @@ package com.gectcr.ece.design.tutorial.networktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class DiscoverActivity extends AppCompatActivity {
+
+    NetworkManager _networkManager;
+    Handler _discoveryHandler;
+    CopyOnWriteArrayList<CharSequence> _discoveredServices;
+    private Spinner _discoveredServicesUIList;
+    public static final String TAG = "DiscoverActivity";
+    private NsdServiceInfo _selectedFromList;
+
+    public class SpinnerListen implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String serviceSelected = parent.getItemAtPosition(position).toString();
+            Log.d(TAG, "selected service is " + serviceSelected);
+            CopyOnWriteArrayList<NsdServiceInfo> allServices = _networkManager.getDiscoveredServices();
+            for(NsdServiceInfo i : allServices) {
+                String name = i.getServiceName() + " " + i.getServiceType();
+                if ( name.equals(serviceSelected) )
+                {
+                    _selectedFromList = i;
+                    break;
+                }
+            }
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            _selectedFromList = null;
+        }
+    }
+
+    private SpinnerListen _discoveredServicesSpinnerListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+        
     }
 }
