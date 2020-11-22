92a0883e259fb2af00ef3e0e6a9e17c74a97dcc8
Harith Manoj <harithpub@gmail.com>
2020-11-22 13:53:13 +0530

    added discover activity for client side

---
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/DiscoverActivity.java
@@ -0,0 +1,14 @@
+package com.gectcr.ece.design.tutorial.networktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiscoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover);
+    }
+}
