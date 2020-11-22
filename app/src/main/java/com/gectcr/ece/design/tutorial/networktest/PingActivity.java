d4b39255c3a52ef2509922a0c5a8f5ecca0e425f
Harith Manoj <harithpub@gmail.com>
2020-11-22 21:20:17 +0530

    added all UI backend methods, placeholder in place of sendmessage

--- app/src/main/java/com/gectcr/ece/design/tutorial/networktest/PingActivity.java
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/PingActivity.java
@@ -3,6 +3,8 @@ package com.gectcr.ece.design.tutorial.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class PingActivity extends AppCompatActivity {
@@ -19,6 +21,7 @@ public class PingActivity extends AppCompatActivity {
     private TextView _inCurrentUI;
     private static final String IN_CURRENT_KEY = "in.curr.ui";
 
+    public static final String TAG = "PingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +46,44 @@ public class PingActivity extends AppCompatActivity {
         }
         _inCurrentUI.setText(in);
         _outCurrentUI.setText(out);
-        
+
 
     }
+
+    public void clickPingHigh(View view) {
+        pingNow(1);
+    }
+
+    public void clickPingLow(View view) {
+        pingNow(0);
+    }
+
+    private void pingNow(int var) {
+
+        if(_outData.length() == 8)  {
+            _outData = _outData.substring(1);
+        }
+        _outData = _outData + String.valueOf(var);
+
+        ((TextView)findViewById(R.id.outData)).setText("Out: " + _outData);
+
+        ((TextView)findViewById(R.id.outCurrent)).setText(String.valueOf(var));
+        Log.i(TAG,"pinging with " + String.valueOf(var));
+        // connection send message
+
+    }
+
+    public void pingMe(int var) {
+
+        if(_inData.length() == 8) {
+            _inData = _inData.substring(1);
+        }
+        _inData = _inData + String.valueOf(var);
+
+        ((TextView)findViewById(R.id.inData)).setText(" In: " + _inData);
+
+        ((TextView)findViewById(R.id.inCurrent)).setText(String.valueOf(var));
+        Log.i(TAG,"pinged with " + String.valueOf(var));
+    }
+}
 }
