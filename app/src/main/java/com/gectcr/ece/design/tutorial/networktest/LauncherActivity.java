38a0649d8cf88f57524f786d6fe9bb37135c413f
Harith Manoj <harithpub@gmail.com>
2020-11-22 12:33:36 +0530

    changed activity names

--- app/src/main/java/com/gectcr/ece/design/tutorial/networktest/LauncherActivity.java
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/LauncherActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NetworkDetect extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_network_detect);
+        setContentView(R.layout.activity_launcher);
     }
 }
