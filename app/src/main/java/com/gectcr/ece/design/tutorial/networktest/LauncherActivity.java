0eb70ecd82eaf5a628776ce94a8ffe0d28faf5bb
Harith Manoj <harithpub@gmail.com>
2020-11-22 12:39:50 +0530

    added empty function for click activities server and client

--- app/src/main/java/com/gectcr/ece/design/tutorial/networktest/LauncherActivity.java
+++ app/src/main/java/com/gectcr/ece/design/tutorial/networktest/LauncherActivity.java
@@ -3,6 +3,7 @@ package com.gectcr.ece.design.tutorial.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+    }
+
+    public void clickLaunchServer(View view) {
+
+    }
+
+    public void clickLaunchClient(View view) {
+
     }
 }
