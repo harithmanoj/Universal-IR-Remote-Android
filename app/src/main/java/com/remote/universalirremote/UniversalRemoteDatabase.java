885fdf755ca30a2e5edf2286ba7a791ca1bcb78c
Harith Manoj <harithpub@gmail.com>
2020-11-25 19:50:01 +0530

    made database class a singleton and added singleton creator / accessor

--- app/src/main/java/com/remote/universalirremote/UniversalRemoteDatabase.java
+++ app/src/main/java/com/remote/universalirremote/UniversalRemoteDatabase.java
@@ -20,12 +20,35 @@
 
 package com.remote.universalirremote;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = {DeviceData.class}, version = 1, exportSchema = true)
 public abstract class UniversalRemoteDatabase extends RoomDatabase {
 
+    public abstract DeviceDataAccess deviceDataAccess();
 
+    private static volatile UniversalRemoteDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static UniversalRemoteDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (UniversalRemoteDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            UniversalRemoteDatabase.class, "UniversalRemoteDB").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 
 }
