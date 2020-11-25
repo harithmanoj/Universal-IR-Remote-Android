d7d172753ef1d07d8bd85c81b7fe628441faa80e
Harith Manoj <harithpub@gmail.com>
2020-11-25 16:31:05 +0530

    added DeviceData class

--- app/src/main/java/com/remote/universalirremote/DeviceData.java
+++ app/src/main/java/com/remote/universalirremote/DeviceData.java
@@ -19,30 +19,5 @@
 
 package com.remote.universalirremote;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NavUtils;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-
-public class DeviceSelect extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_select);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+public class DeviceData {
 }
