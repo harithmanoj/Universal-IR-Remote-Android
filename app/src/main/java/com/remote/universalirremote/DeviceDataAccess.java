6396f36aad681d714fd51f2a6226e3c8efc462cb
Harith Manoj <harithpub@gmail.com>
2020-11-25 18:11:40 +0530

    added class for deviceData Access (DAO) for room API;

--- app/src/main/java/com/remote/universalirremote/DeviceDataAccess.java
+++ app/src/main/java/com/remote/universalirremote/DeviceDataAccess.java
@@ -16,33 +16,10 @@
 //        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 //
+//
 
 package com.remote.universalirremote;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NavUtils;
+public interface DeviceDataAccess {
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
-public class DeviceSelect extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_select);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
