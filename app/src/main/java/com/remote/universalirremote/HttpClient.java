269c6d879797affed88175bfaa1b8af582412fce
Harith Manoj <harithpub@gmail.com>
2020-11-28 23:02:28 +0530

    tentativily completed HttpClient

--- app/src/main/java/com/remote/universalirremote/HttpClient.java
+++ app/src/main/java/com/remote/universalirremote/HttpClient.java
@@ -22,18 +22,18 @@ package com.remote.universalirremote;
 import android.net.nsd.NsdServiceInfo;
 import android.util.Log;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class HttpClient {
-    private NsdServiceInfo _serviceInfo;
+    private final NsdServiceInfo _serviceInfo;
     private HttpURLConnection _httpConnection;
     private String _serviceUrl;
 
@@ -41,19 +41,55 @@ public class HttpClient {
 
     public HttpClient(NsdServiceInfo info) {
         _serviceInfo = info;
-        _serviceUrl = new String("http://") + _serviceInfo.getHost().getHostAddress();
+        _serviceUrl = "http://" + _serviceInfo.getHost().getHostAddress();
     }
 
     public void connect() {
         try {
             URL url = new URL(_serviceUrl);
-            _httpConnection = url.openConnection();
+            _httpConnection = (HttpURLConnection)url.openConnection();
         } catch (MalformedURLException ex) {
             Log.e(TAG, "malformed url exception ", ex);
             ex.printStackTrace();
         } catch (IOException ex) {
             Log.e(TAG, "IO exception ", ex);
             ex.printStackTrace();
+        }
+    }
+
+    public String transaction(Request request) throws IOException {
+        _httpConnection.setDoOutput(true);
+        _httpConnection.setInstanceFollowRedirects(false);
+        _httpConnection.setRequestMethod(request._requestMethod);
+        for(Request.Property i : request._requestProperties) {
+            _httpConnection.setRequestProperty(i._propertyName, i._propertyValue);
+        }
+
+        _httpConnection.connect();
+        if(request._requestMethod.equals("POST")) {
+            OutputStream os = _httpConnection.getOutputStream();
+            os.write(request._postData);
+            os.flush();
+            os.close();
+        }
+
+        int responseCode = _httpConnection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    _httpConnection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            _httpConnection.disconnect();
+            return response.toString();
+        } else {
+            throw new IOException(request._requestMethod +
+                    " response " + ((Integer)responseCode).toString());
         }
     }
 
@@ -69,16 +105,14 @@ public class HttpClient {
             }
         }
 
-        public final String _urlParameters;
         public final byte[] _postData;
         public final String _requestMethod;
         public final ArrayList<Property> _requestProperties;
 
-        public Request(String urlParam, byte[] data, String method, Property... properties) {
-            _urlParameters = urlParam;
+        public Request(byte[] data, String method, Property... properties) {
             _postData = data;
             _requestMethod = method;
-            ArrayList<Property> tempProperties = new ArrayList<Property>(Arrays.asList(properties));
+            ArrayList<Property> tempProperties = new ArrayList<>(Arrays.asList(properties));
             tempProperties.add(new Property("Content-Length", Integer.toString(data.length)));
             _requestProperties = tempProperties;
         }
