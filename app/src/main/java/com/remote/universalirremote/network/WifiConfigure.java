a32d5f46f50c06e969a9646c298de54e13557ac5
Ashwin <aswinrajesh1@gmail.com>
2020-12-06 22:22:21 +0530

    Added configuration response handler

--- app/src/main/java/com/remote/universalirremote/network/WifiConfigure.java
+++ app/src/main/java/com/remote/universalirremote/network/WifiConfigure.java
@@ -6,56 +6,73 @@ import android.os.HandlerThread;
 import android.os.Message;
 import android.util.Log;
 
-import java.util.ArrayList;
-
 public class WifiConfigure {
     public static final String TAG = "WifiConfigure";
-    public static final String _serverIPAddress = "192.168.1.1";
-    public static final String _serverGetURI = "/scan";
-    public static final String _serverSendURI = "/wificonfig";
+    public static final String SERVER_ADDRESS = "192.168.4.1";
+    public static final String SCAN_URI = "/scan";
+    public static final String CONFIG_URI = "/wificonfig";
 
-    public static final String SCAN_KEY = "response.ssids";
+    public static final String SCAN_KEY = "response.scan_key";
 
-    private HttpClient _httpGetClient;
-    private HttpClient _httpSendClient;
+    private HttpClient _httpScanClient;
+    private HttpClient _httpConfigClient;
 
-    private Handler _getResponseHandler;
-    private HandlerThread _getResponseHandlerThread;
+    private Handler _scanResponseHandler;
+    private HandlerThread _scanResponseHandlerThread;
 
-    private Handler _userGetHandler;
-    private Handler _userSendHandler;
+    private Handler _configResponseHandler;
+    private HandlerThread _configResponseHandlerThread;
 
-    public WifiConfigure(Handler handler)
+    private Handler _userScanHandler;
+    private Handler _userUpdateHandler;
+
+    public WifiConfigure(Handler updatehandler, Handler scanHandler)
     {
-        _getResponseHandlerThread = new HandlerThread("ConfigurationResponseHandlerThread");
-        _getResponseHandlerThread.start();
-        _getResponseHandler = new Handler(_getResponseHandlerThread.getLooper()) {
+        _scanResponseHandlerThread = new HandlerThread("ScanResponseHandlerThread");
+        _scanResponseHandlerThread.start();
+        _scanResponseHandler = new Handler(_scanResponseHandlerThread.getLooper()) {
             @Override
             public void handleMessage(Message msg) {
-                String response = msg.getData().getString(_httpGetClient.RESPONSE_KEY);
+                String response = msg.getData().getString(_httpScanClient.RESPONSE_KEY);
 
                 Log.i(TAG, " Received message: " + response);
 
-                String[] ssids = response.split("\\n");
+                String[] ssids = response.split("$");
 
                 Bundle bundle = new Bundle();
                 bundle.putStringArray(SCAN_KEY, ssids);
 
+                for(String s:ssids)
+                    Log.i(TAG, "ssid detected : "+s);
+
                 Message msgresp = new Message();
                 msgresp.setData(bundle);
-                _userGetHandler.sendMessage(msgresp);
+                _userScanHandler.sendMessage(msgresp);
             }
         };
 
-        _httpGetClient = new HttpClient(_serverIPAddress+_serverGetURI, _getResponseHandler);
-        _httpSendClient = new HttpClient(_serverIPAddress+_serverSendURI, _getResponseHandler);
+        _configResponseHandlerThread = new HandlerThread("ConfigurationHandlerThread");
+        _configResponseHandlerThread.start();
+        _configResponseHandler = new Handler(_configResponseHandlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                String response = msg.getData().getString(_httpScanClient.RESPONSE_KEY);
 
-        _userSendHandler = handler;
+                Log.i(TAG, "Received message : " + response);
+                _userUpdateHandler.sendMessage(msg);
+            }
+        };
+
+        _httpScanClient   = new HttpClient(SERVER_ADDRESS+SCAN_URI  , _scanResponseHandler);
+        _httpConfigClient = new HttpClient(SERVER_ADDRESS+CONFIG_URI, _configResponseHandler);
+
+        _userUpdateHandler = updatehandler;
+        _userScanHandler   = scanHandler;
     }
 
     public void getAccessPoints()
     {
-        _httpGetClient.transaction(new HttpClient.Request(
+        _httpScanClient.transaction(new HttpClient.Request(
                 null, "GET",
                 new HttpClient.Request.Property("Content-Type", "application/xml"),
                 new HttpClient.Request.Property("charset", "utf-8"),
@@ -65,7 +82,7 @@ public class WifiConfigure {
 
     public void sendAccessPointData(String ssid, String password)
     {
-        _httpGetClient.transaction(new HttpClient.Request(
+        _httpScanClient.transaction(new HttpClient.Request(
                 (ssid + ":" + password).getBytes(), "POST",
                 new HttpClient.Request.Property("Content-Type", "application/xml"),
                 new HttpClient.Request.Property("charset", "utf-8"),
