b32a735d8075bbe27ba769cb7d0f83483564ed5f
Ashwin <aswinrajesh1@gmail.com>
2020-12-07 11:59:02 +0530

    Added AC test

--- app/src/main/java/com/remote/universalirremote/ui/IRTest.java
+++ app/src/main/java/com/remote/universalirremote/ui/IRTest.java
@@ -13,18 +13,22 @@ import android.view.View;
 
 import com.remote.universalirremote.Constant;
 import com.remote.universalirremote.R;
+import com.remote.universalirremote.network.ACSend;
 import com.remote.universalirremote.network.RawGet;
 import com.remote.universalirremote.network.RawSend;
 
 public class IRTest extends AppCompatActivity {
     private RawGet _irGet;
     private RawSend _irSend;
+    private ACSend _acSend;
 
     private Handler _getHandler;
     private Handler _sendHandler;
+    private Handler _acHandler;
 
     private HandlerThread _getHandlerThread;
     private HandlerThread _sendHandlerThread;
+    private HandlerThread _acHandlerThread;
 
     private String _detectRaw;
     private int _detectProtocol;
@@ -53,7 +57,7 @@ public class IRTest extends AppCompatActivity {
                 _detectRaw = msg.getData().getString(RawGet.RAW_KEY);
                 _detectProtocol = msg.getData().getInt(RawGet.PROTOCOL_KEY);
 
-                Log.i(TAG, "handleMessage: " + _detectRaw + " | " + ((Integer)_detectProtocol).toString());
+                Log.i(TAG, "raw get handleMessage: " + _detectRaw + " | " + ((Integer)_detectProtocol).toString());
             }
         };
         _irGet = new RawGet(serviceInfo, _getHandler);
@@ -64,24 +68,39 @@ public class IRTest extends AppCompatActivity {
             @Override
             public void handleMessage(Message msg) {
                 String resp = msg.getData().getString(RawSend.RESPONSE_KEY);
-                Log.i(TAG, "handleMessage: " + resp);
+                Log.i(TAG, "raw send handleMessage: " + resp);
             }
         };
         _irSend = new RawSend(serviceInfo, _sendHandler);
+
+        _acHandlerThread = new HandlerThread("AC Handler Thread");
+        _acHandlerThread.start();
+        _acHandler = new Handler(_acHandlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                String resp = msg.getData().getString(ACSend.RESPONSE_KEY);
+                Log.i(TAG, "ac handleMessage: " + resp);
+            }
+        };
+        _acSend = new ACSend(serviceInfo, _acHandler);
 
         super.onStart();
     }
 
     public void onGet(View view) {
-        _irGet.getData();
+        _irGet.getData(100);
         Log.i(TAG, "onSend: ");
     }
 
     public void onSend(View view) {
         if(_detectRaw == null)
-            _irSend.sendData("null");
+            _irSend.sendData("null", "null");
         else
-            _irSend.sendData(_detectRaw);
+            _irSend.sendData(_detectRaw, "null");
         Log.i(TAG, "onSend: ");
+    }
+
+    public void onAC(View view) {
+        _acSend.sendData(Constant.Protocols.LG, 1, true, Constant.AcMode.kCool, 25, true, Constant.AcFan.kAuto, Constant.AcSwingv.kAuto, Constant.AcSwingh.kAuto, true, true, true, true, false, true, true, -1, -1, "test");
     }
 }
