5dece512dff136353c1758f750a4dbc057680b71
Harith Manoj <harithpub@gmail.com>
2020-12-09 21:18:44 +0530

    added almost all transmit functionality for genRemote

--- app/src/main/java/com/remote/universalirremote/ui/GenRemoteTransmit.java
+++ app/src/main/java/com/remote/universalirremote/ui/GenRemoteTransmit.java
@@ -18,18 +18,90 @@
 
 package com.remote.universalirremote.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NavUtils;
+
+import com.remote.universalirremote.ApplicationWideSingleton;
 import com.remote.universalirremote.GenRemote;
+import com.remote.universalirremote.database.DeviceButtonConfig;
+import com.remote.universalirremote.network.HttpClient;
+import com.remote.universalirremote.network.RawSend;
+
+import java.net.HttpURLConnection;
 
 public class GenRemoteTransmit extends GenRemote {
+
+    private RawSend _sendRawIrTiming;
+    private HandlerThread _sendResponseThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        super.onCreate(savedInstanceState);
+    }
+    // Menu item selected process
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onStart() {
+        _sendResponseThread = new HandlerThread("RawTvRemoteSendResponse");
+        _sendResponseThread.start();
+        Handler _sendResponse = new Handler(_sendResponseThread.getLooper()) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if (msg.getData().getInt(RawSend.CODE_KEY) != HttpURLConnection.HTTP_OK) {
+                    Toast.makeText(getApplicationContext(),
+                            "button send fail "
+                                    + ((HttpClient.Request.Property) msg.getData()
+                                    .getParcelable(RawSend.POST_META_KEY)).getValue(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+        _sendRawIrTiming = new RawSend(ApplicationWideSingleton.getSelectedService(),
+                _sendResponse);
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        renameOkOrConfig("Config");
+        _deviceButtonConfigRepo.getAllRawData(ApplicationWideSingleton.getSelectedDeviceName());
+        super.onResume();
+    }
+
     @Override
     public void handleButtonClicks(int btnId) {
-
+        DeviceButtonConfig selectedButton = lookupButton(btnId);
+        if(selectedButton == null) {
+            Toast.makeText(getApplicationContext(),
+                    "not configured button", Toast.LENGTH_LONG).show();
+            return;
+        }
+        _sendRawIrTiming.sendData(selectedButton.getIrTimingData(), selectedButton.getDeviceName());
     }
 
     @Override
-    public void clickConfigureOrOK(View view) {
-
+    public void startTransitOrConfigActivity(Intent configIntent, Intent transmitIntent) {
+        startActivity(configIntent);
     }
+
 }
